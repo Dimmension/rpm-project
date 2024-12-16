ddb812828b6e22227343481838d89e4db0d36b2c
slavikyd <slavikyd@gmail.com>
2024-12-17 00:23:15 +0300

    logging v0.1 finished

--- src/handlers/command/start.py
+++ src/handlers/command/start.py
@@ -14,7 +14,7 @@ logging.config.dictConfig(LOGGING_CONFIG)
 
 @router.message(Command('start'))
 async def start_cmd(message: Message, state: FSMContext) -> None:
-    logger.info(f'User {message.user_from} started conversation with bot!')
+    logger.info(f'User {message.from_user} started conversation with bot!')
     await state.set_state(AuthGroup.no_authorized)
     markup = InlineKeyboardMarkup(inline_keyboard=[[buttons.auth]])
     await message.answer(
