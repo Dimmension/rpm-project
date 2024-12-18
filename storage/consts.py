0661770f12e377b0ec36e8d2cf3e489d2f240943
EgorikA4 <egorik4litvinov@mail.ru>
2024-12-18 18:39:08 +0300

    added the ability to change profile

--- storage/consts.py
+++ storage/consts.py
@@ -1,3 +1,4 @@
 EXCHANGE_NAME = 'user_recommendations'
 GENERAL_USERS_QUEUE_NAME = 'user_messages'
 USER_RECOMMENDATIONS_QUEUE_TEMPLATE: str = 'user_recommendations.{user_id}'
+USER_FORM_DATA_QUEUE_TEMPLATE: str = 'user_form_data.{user_id}'
