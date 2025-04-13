e2454f9ea43e382ebaf1935189c6f9da82254dad
slavikyd <slavikyd@gmail.com>
2025-04-14 00:21:17 +0300

    state save fix

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,31 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.fsm.storage.base import KeyBuilder
+from redis.asyncio import Redis
 
 from config.settings import settings
 from src.handlers.command.router import router as command_router
 from src.handlers.message.router import router as message_router
 
-dp = Dispatcher()
+class CustomKeyBuilder(KeyBuilder):
+    def __init__(self, prefix: str = "fsm"):
+        self.prefix = prefix
+        
+    def build(self, key: str, destiny: str) -> str:
+        return f"{self.prefix}:{key}:{destiny}"
+
+storage = RedisStorage(
+    redis=Redis(
+        host='redis',
+        port=6379,
+        db=0
+    ),
+    key_builder=CustomKeyBuilder(prefix="fsm")
+)
+
+dp = Dispatcher(storage=storage)
 default = DefaultBotProperties(parse_mode=ParseMode.HTML)
 bot = Bot(token=settings.BOT_TOKEN, default=default)
 
